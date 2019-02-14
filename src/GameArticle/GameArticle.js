7415df67d4247810b63d1f2b8178cfc1e244a7cb
Mahdi Tehrani <mahdi@frequencydesign.com>
2019-02-14 10:17:07 -0800

    cleaned up code with less jsx blocks and added if checks for titles and added methods for show and hide of article elements

--- src/GameArticle/GameArticle.js
+++ src/GameArticle/GameArticle.js
@@ -1,66 +1,91 @@
 import React, {Component} from 'react';
 import './GameArticle.css';
 
-import { GameContext } from '../SuperBowlLanding/SuperBowlLanding.js';
-import superBowls from "../data/superBowls";
-import Match from "../Match/Match";
+import {GameContext} from '../SuperBowlLanding/SuperBowlLanding.js';
+// import superBowls from "../data/superBowls";
+// import Match from "../Match/Match";
 // import gameContent from "../data/content";
 
 class GameArticle extends Component {
+    state = {
+        articleClosed: true
+    }
 
     articleCloseToggle = () => {
-
+        this.setState({articleClosed: true});
     }
 
     articleReadMore = () => {
-
+        // this.setState({articleClosed: !this.state.articleClosed});
+        this.setState({articleClosed: false});
     }
 
     render() {
         return (
             <GameContext.Consumer>
-                {(context) => this.props.articleType == 'firstHand' ? (
-                    <div className="article" tabIndex="-1" title={`${context.state.gameContent.content.firstHand.title}`}>
+                {(context) => (
+                    <div className="article"
+                         tabIndex="-1"
+                         title={this.props.articleType === 'firstHand' ?
+                             `${context.state.gameContent.content.firstHand.title}`
+                             :
+                             `${context.state.gameContent.content.storyOfTheGame.title}`
+                         }
+                    >
                         <div className="article-header">
-                            <button className="collapse-text-button collapse" ng-click="showHide()">X</button>
-                            <button className="collapse-text-button-absolute collapse">X</button>
+                            <button className="collapse-text-button-absolute collapse"
+                                    onClick={this.articleCloseToggle}
+                                    style={this.state.articleClosed ? {display: 'none'} : null}
+                            >
+                                X
+                            </button>
                             <h2>
-                                {context.state.gameContent.content.firstHand.title}
+                                {this.props.articleType === 'firstHand' ?
+                                    `${context.state.gameContent.content.firstHand.title}`
+                                    :
+                                    `${context.state.gameContent.content.storyOfTheGame.title}`
+                                }
                                 <div className="gold-bottom-border"></div>
                             </h2>
                         </div>
-                        <game-article-content text="text" show-hide="showHide()"></game-article-content>
-                        <div>
-                            {context.state.gameContent.content.firstHand.text.map( (paragraph, i) =>
-                                <p className="article-content" key={i}>
-                                    <b className="bolded-first-text">{paragraph.intro}</b>
-                                    {paragraph.text}
-                                </p>
-                            )}
-                        </div>
-                        <button className="read-more" id="readMoreButton"><span
-                            className="read-more-text">read more...</span></button>
-                    </div>
-                ) : (
-                    <div className="article" tabIndex="-1" title={`${context.state.gameContent.content.storyOfTheGame.title}`}>
-                        <div className="article-header">
-                            <button className="collapse-text-button collapse" ng-click="showHide()">X</button>
-                            <button className="collapse-text-button-absolute collapse">X</button>
-                            <h2>
-                                {context.state.gameContent.content.storyOfTheGame.title}
-                                <div className="gold-bottom-border"></div>
-                            </h2>
-                        </div>
-                        <game-article-content text="text" show-hide="showHide()"></game-article-content>
-                        <div>
-                            {context.state.gameContent.content.storyOfTheGame.text.map( (paragraph, i) =>
-                                <p className="article-content" key={i}>
-                                    {paragraph.text}
-                                </p>
-                            )}
-                        </div>
-                        <button className="read-more" id="readMoreButton"><span
-                            className="read-more-text">read more...</span></button>
+
+                        {this.props.articleType === 'firstHand' ? (
+
+                            <div>
+                                {context.state.gameContent.content.firstHand.text.map((paragraph, i) =>
+                                    <p className="article-content"
+                                       style={this.state.articleClosed ? {display: 'none'} : null}
+                                       key={i}
+                                    >
+                                        <b className="bolded-first-text">{paragraph.intro}</b>
+                                        {paragraph.text}
+                                    </p>
+                                )}
+                            </div>
+
+                        ) : (
+
+                            <div>
+                                {context.state.gameContent.content.storyOfTheGame.text.map((paragraph, i) =>
+                                    <p className="article-content"
+                                       style={this.state.articleClosed ? {display: 'none'} : null}
+                                       key={i}
+                                    >
+                                        {paragraph.text}
+                                    </p>
+                                )}
+                            </div>
+
+                        )
+                        }
+
+                        <button className="read-more"
+                                id="readMoreButton"
+                                onClick={this.articleReadMore}
+                                style={this.state.articleClosed ? null : {display: 'none'}}
+                        >
+                            <span className="read-more-text">read more...</span>
+                        </button>
                     </div>
                 )}
             </GameContext.Consumer>
